ef047922b51747811423694b5749fa8b04ab6600
Matheus Couto <matheuscouto.tds2.senai@gmail.com>
2025-03-11 09:33:55 -0300

    feat: create sidebar components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,15 @@
 import Header from "@/components/header";
 import styles from "./page.module.css";
+import Sidebar from "@/components/sidebar";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <main class="dashboardContainer">
+        <Sidebar />
+      </main>
     </div>
   );
 }
