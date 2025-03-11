f272f440a6cb20f6861f953029f4b01be8e954de
Matheus Couto <matheuscouto.tds2.senai@gmail.com>
2025-03-11 11:01:40 -0300

    create dashboard component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,16 @@
 import Header from "@/components/header";
 import styles from "./page.module.css";
 import Sidebar from "@/components/sidebar";
+import DashboardContent from "@/components/dashboardContent";
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-      <main class="dashboardContainer">
+      <main className={styles.dashboardContainer}>
         <Sidebar />
+        <DashboardContent />
       </main>
     </div>
   );
